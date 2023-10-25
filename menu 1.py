970a0ce3f60b0e62204b0ff79e738664c0a69703
Jonas Ribeiro <jonas.ribeiro01@fatec.sp.gov.br>
2023-10-25 17:06:38 -0300

    terminando crud das entidades de Mercado Livre

--- menu 1.py
+++ menu 1.py
@@ -1,35 +1,21 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://jonas:llDE5vpTU2h2RSbC@nosql.ocxhdgk.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+uri = "mongodb+srv://jonas:PCpyir20UvSL2v40@nosql.ocxhdgk.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
 
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 global db
 db = client.biblioteca
 
-def delete_usuario(nome, sobrenome):
-    #Delete
-    global db
-    mycol = db.usuario
-    myquery = {"nome": nome, "sobrenome":sobrenome}
-    mydoc = mycol.delete_one(myquery)
-    print("Deletado o usuário ", mydoc)
-
-def delete_vendedor(nome, sobrenome):
-    #Delete
-    global db
-    mycol = db.usuario
-    myquery = {"nome": nome, "sobrenome":sobrenome}
-    mydoc = mycol.delete_one(myquery)
-    print("Deletando o vendedor ", mydoc)
+""" USUARIO """
 
 def create_usuario():
-    #Insert
     global db
     mycol = db.usuario
     print("\nInserindo um novo usuário")
     nome = input("Nome: ")
+    senha = input("Senha: ")
+    email = input("Email: ")
     sobrenome = input("Sobrenome: ")
     cpf = input("CPF: ")
     key = 1
@@ -41,7 +27,7 @@ def create_usuario():
         cidade = input("Cidade: ")
         estado = input("Estado: ")
         cep = input("CEP: ")
-        endereco = {        #isso nao eh json, isso é chave-valor, eh um obj
+        endereco = {
             "rua":rua,
             "num": num,
             "bairro": bairro,
@@ -49,14 +35,62 @@ def create_usuario():
             "estado": estado,
             "cep": cep
         }
-        end.append(endereco) #estou inserindo na lista
+        end.append(endereco)
         key = input("Deseja cadastrar um novo endereço (S/N)? ")
-    mydoc = { "nome": nome, "sobrenome": sobrenome, "cpf": cpf, "end": end }
+    mydoc = { "nome": nome, "sobrenome": sobrenome, "cpf": cpf, "senha": senha, "email": email, "end": end }
     x = mycol.insert_one(mydoc)
     print("Documento inserido com ID ", x.inserted_id)
 
+def read_usuario(nome):
+    global db
+    mycol = db.usuario
+    print("Usuários existentes: ")
+    if not len(nome):
+        mydoc = mycol.find().sort("nome")
+        for x in mydoc:
+            print(x["nome"], x["cpf"])
+    else:
+        myquery = {"nome": nome}
+        mydoc = mycol.find(myquery)
+        for x in mydoc:
+            print(x)
+
+def update_usuario(nome):
+    global db
+    mycol = db.usuario
+    myquery = {"nome": nome}
+    mydoc = mycol.find_one(myquery)
+    print("Dados do usuário: ", mydoc)
+
+    nome = input("Mudar Nome:")
+    if len(nome):
+        mydoc["nome"] = nome
+
+    sobrenome = input("Mudar Sobrenome:")
+    if len(sobrenome):
+        mydoc["sobrenome"] = sobrenome
+
+    email = input("Mudar Email:")
+    if len(sobrenome):
+        mydoc["email"] = email
+
+    cpf = input("Mudar CPF:")
+    if len(cpf):
+        mydoc["cpf"] = cpf
+
+    newvalues = { "$set": mydoc }
+    mycol.update_one(myquery, newvalues)
+
+def delete_usuario(nome, sobrenome):
+    global db
+    mycol = db.usuario
+    myquery = {"nome": nome, "sobrenome":sobrenome}
+    mydoc = mycol.delete_one(myquery)
+    print("Deletado o usuário ", mydoc)
+
+""" VENDEDOR """
+
 def create_vendedor():
-    #Insert
     global db
     mycol = db.vendedor
     print("\nInserindo um novo vendedor")
@@ -74,7 +108,7 @@ def create_vendedor():
         cidade = input("Cidade: ")
         estado = input("Estado: ")
         cep = input("CEP: ")
-        endereco = {        #isso nao eh json, isso é chave-valor, eh um obj
+        endereco = {
             "rua":rua,
             "num": num,
             "bairro": bairro,
@@ -88,11 +122,10 @@ def create_vendedor():
     x = mycol.insert_one(mydoc)
     print("Documento inserido com ID ", x.inserted_id)
 
-def read_usuario(nome):
-    #Read
+def read_vendendor(nome):
     global db
-    mycol = db.usuario
-    print("Usuários existentes: ")
+    mycol = db.vendedor
+    print("Vendedores existentes: ")
     if not len(nome):
         mydoc = mycol.find().sort("nome")
         for x in mydoc:
@@ -103,13 +136,13 @@ def read_usuario(nome):
         for x in mydoc:
             print(x)
 
-def update_usuario(nome):
-    #Read
+def update_vendedor(nome):
     global db
-    mycol = db.usuario
+    mycol = db.vendedor
     myquery = {"nome": nome}
     mydoc = mycol.find_one(myquery)
-    print("Dados do usuário: ", mydoc)
+    print("Dados do vendedor: ", mydoc)
+
     nome = input("Mudar Nome:")
     if len(nome):
         mydoc["nome"] = nome
@@ -118,12 +151,151 @@ def update_usuario(nome):
     if len(sobrenome):
         mydoc["sobrenome"] = sobrenome
 
+    email = input("Mudar Email:")
+    if len(sobrenome):
+        mydoc["email"] = email
+
     cpf = input("Mudar CPF:")
     if len(cpf):
         mydoc["cpf"] = cpf
 
+    cnpj = input("Mudar CNPJ:")
+    if len(cnpj):
+        mydoc["cnpj"] = cnpj
+
     newvalues = { "$set": mydoc }
     mycol.update_one(myquery, newvalues)
+
+def delete_vendedor(nome, sobrenome):
+    global db
+    mycol = db.vendedor
+    myquery = {"nome": nome, "sobrenome":sobrenome}
+    mydoc = mycol.delete_one(myquery)
+    print("Deletando o vendedor ", mydoc)
+
+""" PRODUTO """
+
+def create_produto():
+    global db
+    mycol = db.produto
+    print("\nInserindo um novo produto")
+    nome = input("Nome do produto: ")
+    preco = float(input("Preço do produto: "))
+    descricao = input("Descrição do produto: ")
+    categoria = input("Categoria do produto: ")
+    mydoc = { "nome": nome, "preco": preco, "descricao": descricao, "categoria": categoria }
+    x = mycol.insert_one(mydoc)
+    print("Produto inserido com ID ", x.inserted_id)
+
+def read_produto(nome):
+    global db
+    mycol = db.produto
+    print("Produtos existentes: ")
+    if not len(nome):
+        mydoc = mycol.find().sort("nome")
+        for x in mydoc:
+            print(x["nome"], x["categoria"], x["preco"])
+    else:
+        myquery = {"nome": nome}
+        mydoc = mycol.find(myquery)
+        for x in mydoc:
+            print(x)
+
+def update_produto(nome):
+    global db
+    mycol = db.produto
+    myquery = {"nome": nome}
+    mydoc = mycol.find_one(myquery)
+    print("Dados do produto: ", mydoc)
+
+    nome = input("Mudar Nome do produto:")
+    if len(nome):
+        mydoc["nome"] = nome
+
+    preco = float(input("Mudar Preço do produto:"))
+    if preco:
+        mydoc["preco"] = preco
+
+    descricao = input("Mudar Descrição do produto:")
+    if len(descricao):
+        mydoc["descricao"] = descricao
+
+    categoria = input("Mudar Categoria do produto:")
+    if len(categoria):
+        mydoc["categoria"] = categoria
+
+    newvalues = { "$set": mydoc }
+    mycol.update_one(myquery, newvalues)
+
+def delete_produto(nome):
+    global db
+    mycol = db.produto
+    myquery = {"nome": nome}
+    mydoc = mycol.delete_one(myquery)
+    print("Produto deletado com ID ", mydoc.deleted_id)
+
+""" COMPRA """
+
+def create_compra():
+    global db
+    mycol = db.compra
+    print("\nInserindo uma nova compra")
+    data_compra = input("Data da compra (DD-MM-AAAA): ")
+    data_entrega = input("Data de entrega (DD-MM-AAAA): ")
+    status_compra = input("Status da compra: ")
+    usuario_id = input("ID do usuário que realizou a compra: ")
+    produto_id = input("ID do produto comprado: ")
+    
+    mydoc = {"data_compra": data_compra, "data_entrega": data_entrega, "status_compra": status_compra, "usuario_id": usuario_id, "produto_id": produto_id}
+    
+    x = mycol.insert_one(mydoc)
+    print("Compra inserida com ID ", x.inserted_id)
+
+def read_compra(compra_id):
+    global db
+    mycol = db.compra
+    print("Compras existentes: ")
+    if not len(compra_id):
+        mydoc = mycol.find().sort("data_compra")
+        for x in mydoc:
+            print(x["compra_id"], x["data_compra"])
+    else:
+        myquery = {"compra_id": compra_id}
+        mydoc = mycol.find(myquery)
+        for x in mydoc:
+            print(x)
+
+def update_compra(compra_id):
+    global db
+    mycol = db.compra
+    myquery = {"compra_id": compra_id}
+    mydoc = mycol.find_one(myquery)
+    print("Dados da compra: ", mydoc)
+
+    data_compra = input("Mudar Data da compra (DD-MM-AAAA):")
+    if len(data_compra):
+        mydoc["data_compra"] = data_compra
+
+    data_entrega = input("Mudar Data de entrega (DD-MM-AAAA):")
+    if len(data_entrega):
+        mydoc["data_entrega"] = data_entrega
+
+    status_compra = input("Mudar Status da compra:")
+    if len(status_compra):
+        mydoc["status_compra"] = status_compra
+
+    newvalues = { "$set": mydoc }
+    mycol.update_one(myquery, newvalues)
+
+def delete_compra(compra_id):
+    global db
+    mycol = db.compra
+    myquery = {"compra_id": compra_id}
+    mydoc = mycol.delete_one(myquery)
+    print("Compra deletada com ID ", mydoc.deleted_id)
+
+
+""" CLI """
 
 key = 0
 sub = 0
@@ -131,6 +303,7 @@ while (key != 'S'):
     print("1-CRUD Usuário")
     print("2-CRUD Vendedor")
     print("3-CRUD Produto")
+    print("4-CRUD Comprar")
     key = input("Digite a opção desejada? (S para sair) ")
 
     if (key == '1'):
@@ -161,11 +334,21 @@ while (key != 'S'):
     elif (key == '2'):
         print("Menu do Vendedor")
         print("1-Create Vendedor")
+        print("2-Read Vendedor")
+        print("3-Update Vendedor")
         print("4-Delete Vendedor")
         sub = input("Digite a opção desejada? (V para voltar) ")
         if (sub == '1'):
             print("Create Vendedor")
             create_vendedor()
+
+        elif (sub == '2'):
+            nome = input("Read usuário, deseja algum nome especifico? ")
+            read_vendendor(nome)
+        
+        elif (sub == '3'):
+            nome = input("Update usuário, deseja algum nome especifico? ")
+            update_vendedor(nome)
 
         elif (sub == '4'):
             print("Delete Vendedor")
@@ -173,7 +356,46 @@ while (key != 'S'):
             sobrenome = input("Sobrenome a ser deletado: ")
             delete_vendedor(nome, sobrenome)
 
-    elif (key == '3'):
-        print("Menu do Produto")        
+    elif key == '3':
+        print("Menu do Produto")
+        print("1. Create Produto")
+        print("2. Read Produto")
+        print("3. Update Produto")
+        print("4. Delete Produto")
+        sub = input("Digite a opção desejada? (V para voltar) ")
+        if sub == '1':
+            print("Create Produto")
+            create_produto()
+        elif sub == '2':
+            nome = input("Read Produto, deseja algum nome específico? ")
+            read_produto(nome)
+        elif sub == '3':
+            nome = input("Update Produto, deseja algum nome específico? ")
+            update_produto(nome)
+        elif sub == '4':
+            print("Delete Produto")
+            nome = input("Nome a ser deletado: ")
+            delete_produto(nome)
 
-print("Tchau Prof...")
+    elif key == '4':
+        print("Menu da Compra")
+        print("1. Create Compra")
+        print("2. Read Compra")
+        print("3. Update Compra")
+        print("4. Delete Compra")
+        sub = input("Digite a opção desejada? (V para voltar) ")
+        if sub == '1':
+            print("Create Compra")
+            create_compra()
+        elif sub == '2':
+            compra_id = input("Read Compra, deseja algum ID específico? ")
+            read_compra(compra_id)
+        elif sub == '3':
+            compra_id = input("Update Compra, deseja algum ID específico? ")
+            update_compra(compra_id)
+        elif sub == '4':
+            print("Delete Compra")
+            compra_id = input("ID a ser deletado: ")
+            delete_compra(compra_id)
+    else:
+        print("Tchau Prof...")
